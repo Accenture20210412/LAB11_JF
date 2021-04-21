7b4c8429597ccd6a81fb66a4d35b3d8347377f26
Jakub Fik <fikkuba@gmail.com>
2021-04-21 10:17:12 +0200

    test 1A

---
+++ src/test/java/liblary/naming/LibraryManagerTest.java
@@ -0,0 +1,28 @@
+package liblary.naming;
+
+
+import org.junit.jupiter.api.Assumptions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LibraryManagerTest {
+    LibraryFactory libraryFactory = new LibraryFactory();
+    LibraryManager libraryManager;
+
+    @BeforeEach
+    void init() {
+        libraryManager = libraryFactory.library();
+    }
+
+    @Test
+    void shouldAddBookToLiblary() {
+        Book book = new Book( ISBN.of("1234")
+                , "Juliusz Słowacki"
+                , "Balladyna");
+        libraryManager.putBook(book);
+        assertEquals(1, libraryManager.getBookAmounts(book));
+    }
+
+}
